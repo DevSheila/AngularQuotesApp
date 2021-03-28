2509c71e78b6a55f1ae0b1fa04ec269912db21c2
sheila <devsheilawambui@gmail.com>
2021-03-29 00:51:41 +0300

    created model class for quotes

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,2 +1,24 @@
+// import { ɵBrowserPlatformLocation } from '@angular/platform-browser';
+
 export class Quote {
+    showInfo: boolean;
+    // id
+    // name
+    // title
+    // quote
+    // author
+    // date posted 
+    // likes
+    // dislikes
+  constructor(public id:number,
+    public name:string,
+    public quote:string,
+    public author:string, 
+    public datePosted: Date,
+    public likes:number,
+     public dislikes:number)
+     {
+
+      this.showInfo=false;
+    }
 }
