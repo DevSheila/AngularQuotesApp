30cbe85f78a07e513b5a0e9d49be58dec53d6d77
sheila <devsheilawambui@gmail.com>
2021-03-29 06:46:47 +0300

    edited app module file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,28 @@
 import { NgModule } from '@angular/core';
+import{ FormsModule} from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { QuotesComponent } from './quotes/quotes.component';
-import { QuotesDetailsComponent } from './quotes-details/quotes-details.component';
-import { QuoteDetailsComponent } from './quote-details/quote-details.component';
 import { QuotesFormComponent } from './quotes-form/quotes-form.component';
-import { DatePipePipe } from './date-pipe.pipe';
+import { QuotesComponent } from './quotes/quotes.component';
+import { QuoteDetailsComponent } from './quote-details/quote-details.component';
 import { HighlightQuoteDirectiveDirective } from './highlight-quote-directive.directive';
+import { DatePipePipe } from './date-pipe.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    QuotesComponent,
-    QuotesDetailsComponent,
-    QuoteDetailsComponent,
     QuotesFormComponent,
-    DatePipePipe,
-    HighlightQuoteDirectiveDirective
+    QuotesComponent,
+    QuoteDetailsComponent,
+    HighlightQuoteDirectiveDirective,
+    DatePipePipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -8,27 +8,24 @@ import { Quote } from '../quote';
   styleUrls: ['./quotes.component.css']
 })
 export class QuotesComponent implements OnInit {
-  title = 'Quoty Corner';
+   title = 'Quoty Corner';
   quotes:Quote[] = [
     new Quote (1,'Nick Viche','The greatest glory in living lies not in never falling, but in rising every time we fall.','Nelson Mandela', new Date(2020,7,12),0,0),
     new Quote (2,'Travis Baker',"Your time is limited, so don't waste it living someone else's life. Don't be trapped by dogma – which is living with the results of other people's thinking.",'Steve Jobs',new Date(2019,6,3),0,0),
     new Quote (3,'Harry Potter',"If you look at what you have in life, you'll always have more. If you look at what you don't have in life, you'll never have enough",'Oprah Winfrey',new Date(2020,7,17),0,0),
     new Quote (4,'Ginny Sturgis',"If you set your goals ridiculously high and it's a failure, you will fail above everyone else's success.",'James Cameron',new Date(2020,7,15),0,0)
   ];
-
   get sortQuotes() {
     return this.quotes.sort((a, b) => {
       return <any>new Date(b.datePosted) - <any>new Date(a.datePosted);
     });
   }  
-
   addedQuote(quote:any){
     let arraysize = this.quotes.length;
     quote.id = arraysize+1;
     quote.completeDate = new Date(quote.completeDate)
     this.quotes.push(quote)
   }
-
   quoteDelete(isRead:any, index:any){
     if (isRead) {
       let toDelete = confirm(`Sure you wanna delete this Quote?`)
@@ -38,7 +35,7 @@ export class QuotesComponent implements OnInit {
       
     }
   }
-
+ 
   displayInfo(index:any){
     this.quotes[index].showInfo = !this.quotes[index].showInfo;
   }
